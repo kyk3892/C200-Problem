cb42fdb86f76180fdf9d5c6c084f0b20b7c95e74
kyk3892 <kyk3892@naver.com>
2020-05-16 18:28:24 +0900

    노트북 커밋

--- C200problem/Program.cs
+++ C200problem/Program.cs
@@ -8,6 +8,7 @@ namespace C200problem
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("잘되는지 확인");
+            Console.WriteLine("노트북 수정도 확인");
         }
     }
 }
